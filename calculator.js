6d20bc9aba908ea9f1f29c5a72248d73060e9d0e
paria-zgh <paria.zoghi73@yahoo.com>
2025-02-27 00:06:54 -0800

    اولین کامیت

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function calculator(){
+ let x="";
+ let y="";
+let z="";
+ document.getElementById("demo").value=z;
+}
+const seven=()=>{
+  x=7;
+  document.getElementById("demo").value=x;
+}
+const eight=()=>{
+  y=8
+  document.getElementById("demo").value=x;
+}
+const zarb=()=>{
+  z=x*y;
+    document.getElementById("demo").value=z;
+  
+}
